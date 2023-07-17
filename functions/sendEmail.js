be3571334e6d7ea12845fdcfdd8a6256906545c0
Muhanaawahyu <muhanaawahyu00@gmail.com>
2023-07-17 11:39:30 +0700

    test

--- functions/sendEmail.js
+++ functions/sendEmail.js
@@ -2,7 +2,7 @@ import { microgen } from "../libs/axios.js";
 
 const sendEmail = async (req, res) => {
     console.log(req, res)
-    return (req)
+    return res.status(200).json(req)
 }
 
 export {
